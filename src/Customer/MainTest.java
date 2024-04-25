ac336a34fbc3843a3af93a9de7d3aaf542c22f79
Arul6851 <12304.arul@gmail.com>
2024-04-26 01:41:44 +0530

    Register Page

--- src/Customer/MainTest.java
+++ src/Customer/MainTest.java
@@ -5,14 +5,18 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
-public class LoginTest {
+public class MainTest {
     private WebDriver driver;
     private LoginPage loginPage;
     private String baseUrl = "http://localhost:5173"; // Change this to your app's URL
     private BookingPage bookingPage;
     private ConfirmationPage confirmPage;
+    private RegisterPage registerPage;
     
     @BeforeClass
     public void setUp() {
@@ -22,18 +26,47 @@ public class LoginTest {
         loginPage = new LoginPage(driver);
         bookingPage = new BookingPage(driver);
         confirmPage = new ConfirmationPage(driver);
+        registerPage = new RegisterPage(driver);
     }
 
     @DataProvider(name = "loginData")
     public Object[][] getLoginData() {
         return new Object[][] {
-            {"jovi", "jovi@123"},
-            {"invalid_name","invalid_password"},
-            {"",""}
+            {"arul", "arul@123"},
+            {"jovi","jovi@123"},
+            {"Invalid","invalid"}
+            
+        };
+    }
+    
+    @DataProvider(name = "registerData")
+    public Object[][] getRegisterData() {
+        return new Object[][] {
+            {"John Doe", "John", "john@doe.com", "password123", "1234567890", 0},
+            {"Jane Smith", "Jane", "jane@smith.com", "password456", "0987654321", 0},
+            {"Invalid User", "Invalid", "invalid@email", "invalid", "1234567890", 0}
         };
     }
 
-    @Test(dataProvider = "loginData", priority=0)
+    @Test(dataProvider = "registerData", priority=0)
+    public void testRegister(String name, String username, String email, String password, String phone, int typeIndex) throws InterruptedException {
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[2]/a")).click();
+        Thread.sleep(3000);
+        System.out.println("Entering the credentials for registration");
+        registerPage.setName(name);
+        registerPage.setUsername(username);
+        registerPage.setEmail(email);
+        registerPage.setPassword(password);
+        registerPage.setPhone(phone);
+        registerPage.setType(typeIndex);
+        registerPage.clickRegisterButton();
+        Thread.sleep(3000);
+        Assert.assertEquals(driver.getCurrentUrl(), baseUrl);
+        System.out.println("After successful register, redirected to login page.");
+    }
+
+
+    @Test(dataProvider = "loginData", priority=1)
     public void testLogin(String username, String password) throws InterruptedException{
     	System.out.println("Entering the credentials for login");
         loginPage.setUsername(username);
@@ -44,7 +77,7 @@ public class LoginTest {
         System.out.println("After successful login, redirected to home");
     }
     
-    @Test(priority=1)
+    @Test(priority=2)
     public void testBooking() throws InterruptedException{
 		Thread.sleep(2000);
 		System.out.println("Booking a service");
@@ -62,7 +95,7 @@ public class LoginTest {
         
     }
     
-    @Test(priority=2)
+    @Test(priority=3)
     public void testBookingRequest() throws InterruptedException{
     	JavascriptExecutor js = (JavascriptExecutor)driver;
     	js.executeScript("scrollBy(0, 2500)");
